e21a00e4ed544375562eaefc840fdfa3c132c638
Nathan Duncan <nathanduncan@Nathans-MacBook-Pro.local>
2020-12-17 14:07:49 +0000

    Setup startRemoveExpense

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -41,6 +41,20 @@ export const removeExpense = ({ id } = {}) => ({
   id
 });
 
+export const startRemoveExpense = ({ id } = {}) => {
+  return (dispatch) => {
+  return database.ref(`expenses/${id}`).remove().then((ref) => {
+    dispatch(removeExpense({
+      id
+    }));
+  });
+ };
+};
+
+// link to database, use set to remove. - look at firebase file.
+
+
+
 // EDIT_EXPENSE
 export const editExpense = (id, updates) => ({
   type: 'EDIT_EXPENSE',
